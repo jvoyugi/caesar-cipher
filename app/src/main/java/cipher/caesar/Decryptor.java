1f744d051c747d518618989bdb3ceda7424bad20
Javan Oyugi <jvoyugi@outlook.com>
2023-04-07 00:36:22 +0300

    remove prints

--- app/src/main/java/cipher/caesar/Decryptor.java
+++ app/src/main/java/cipher/caesar/Decryptor.java
@@ -14,7 +14,6 @@ public class Decryptor {
     public String decode() {
         char[] cipherTextCharArray = cipherText.toCharArray();
         char[] plainTextCharArray = new char[cipherTextCharArray.length];
-        System.out.println(this.cipherText);
         for (int index = 0; index < cipherTextCharArray.length; index++) {
             char cipherLetter = cipherTextCharArray[index];
             char letter = cipherLetter > 'Z' || cipherLetter < 'A' ? cipherLetter
