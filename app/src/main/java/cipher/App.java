7f5b356ecb638ad22c2af8abccc90ad03c67ebe6
Javan Oyugi <jvoyugi@outlook.com>
2023-04-07 00:43:30 +0300

    use class variable for planText

--- app/src/main/java/cipher/App.java
+++ app/src/main/java/cipher/App.java
@@ -7,4 +7,6 @@ public class App {
 
     public static void main(String[] args) {
     }
+
+    
 }
--- app/src/test/java/cipher/caesar/EncryptorTest.java
+++ app/src/test/java/cipher/caesar/EncryptorTest.java
@@ -4,9 +4,16 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EncryptorTest {
+
+   private String plainText;
+
+   @Before
+   public void setUp() {
+      plainText = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+   }
+
    @Test
    public void encryptorEncode_withKey7() {
-      String plainText = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
       String cipherText = "AOL XBPJR IYVDU MVE QBTWZ VCLY AOL SHGF KVN";
       int key = 7;
       Encryptor encryptor = new Encryptor(key, plainText);
@@ -15,7 +22,6 @@ public class EncryptorTest {
 
    @Test
    public void encryptorEncode_withKey7_and_mustAlwaysBeInCapital() {
-      String plainText = "tHE QUICK BRoWN FOX JUMPs OVER THe LaZY DOG";
       String cipherText = "AOL XBPJR IYVDU MVE QBTWZ VCLY AOL SHGF KVN";
       int key = 7;
       Encryptor encryptor = new Encryptor(key, plainText);
@@ -24,7 +30,6 @@ public class EncryptorTest {
 
    @Test
    public void encryptorEncode_withNegativeKey_and_mustAlwaysBeInCapital() {
-      String plainText = "tHE QUICK BRoWN FOX JUMPs OVER THe LaZY DOG";
       String cipherText = "OCZ LPDXF WMJRI AJS EPHKN JQZM OCZ GVUT YJB";
       int key = -5;
       Encryptor encryptor = new Encryptor(key, plainText);
@@ -33,7 +38,6 @@ public class EncryptorTest {
 
    @Test
    public void encryptorEncode_withMidNegativeKey_and_mustAlwaysBeInCapital() {
-      String plainText = "tHE QUICK BRoWN FOX JUMPs OVER THe LaZY DOG";
       String cipherText = "UIF RVJDL CSPXO GPY KVNQT PWFS UIF MBAZ EPH";
       int key = -25;
       Encryptor encryptor = new Encryptor(key, plainText);
@@ -42,7 +46,6 @@ public class EncryptorTest {
 
    @Test
    public void encryptorEncode_withLargeKey_and_mustAlwaysBeInCapital() {
-      String plainText = "tHE QUICK BRoWN FOX JUMPs OVER THe LaZY DOG";
       String cipherText = "VJG SWKEM DTQYP HQZ LWORU QXGT VJG NCBA FQI";
       int key = 50000;
       Encryptor encryptor = new Encryptor(key, plainText);
@@ -51,12 +54,9 @@ public class EncryptorTest {
 
    @Test
    public void encryptorEncode_withLargeNegativeKey_and_mustAlwaysBeInCapital() {
-      String plainText = "tHE QUICK BRoWN FOX JUMPs OVER THe LaZY DOG";
       String cipherText = "NBY KOCWE VLIQH ZIR DOGJM IPYL NBY FUTS XIA";
       int key = -20000;
       Encryptor encryptor = new Encryptor(key, plainText);
       assertEquals(encryptor.encode(), cipherText);
    }
-
-   
 }
